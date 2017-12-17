ca41e5ef2e1e0f80c47016a5fa60a25b7922b3fd
Andy608 <AndrewR608@gmail.com>
2017-12-16 20:49:35 -0500

    Migrating over to other computer since this screen is broken.

---
+++ src/com/brickblock/graphics/resource/shader/ShaderProgram.h
@@ -0,0 +1,19 @@
+#ifndef BB_SHADERPROGRAM_H_
+#define BB_SHADERPROGRAM_H_
+
+#include "../Resource.h"
+
+namespace bb
+{
+	class ShaderProgram : public Resource
+	{
+	public:
+		ShaderProgram();
+		~ShaderProgram();
+
+	private:
+
+	};
+}
+
+#endif 
